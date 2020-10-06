a0c990274560bb057f2bf190efa888382abc7419
leeye51456 <leeye51456@gmail.com>
2020-10-07 04:25:57 +0900

    Update twb to be able to build specified files

--- scripts/tailwindcss-build.js
+++ scripts/tailwindcss-build.js
@@ -1,50 +1,57 @@
 const glob = require('glob');
 const { spawnSync } = require('child_process');
 
-function buildIndex() {
-  const fileNames = glob.sync('src/tailwind.css');
-  if (fileNames.length === 0) {
-    return false;
-  }
+function printError(message) {
+  console.error(`tailwindcss-build.js: ${message}`);
+}
 
-  console.error(`tailwindcss-build.js: Building "${fileNames[0]}" into "src/index.css".`);
-  const build = spawnSync('yarn', ['tailwindcss', 'build', fileNames[0], '-o', 'src/index.css']);
+function build(src, dst) {
+  printError(`Building "${src}" into "${dst}".`);
+  const build = spawnSync('yarn', ['tailwindcss', 'build', src, '-o', dst]);
   if (build.status !== 0) {
-    console.error(`tailwindcss-build.js: Failed to build "${fileNames[0]}" into "src/index.css".`);
+    printError(`Failed to build "${src}" into "${dst}".`);
     return false;
   }
-
   return true;
 }
 
-function buildOthers() {
-  const fileNames = glob.sync('src/**/*.tailwind.css');
-  const outputNames = fileNames.map((fileName) => fileName.replace(/\.tailwind\.css$/i, '.css'));
+function tryBuild(fileNames) {
+  const indexPattern = /(?<=^|\/)tailwind\.css$/i;
+  const normalPattern = /(?<=.+\.)tailwind\.css$/i;
 
-  for (let i = 0; i < fileNames.length; i += 1) {
-    console.error(`tailwindcss-build.js: Building "${fileNames[i]}" into "${outputNames[i]}".`);
-    const build = spawnSync('yarn', ['tailwindcss', 'build', fileNames[i], '-o', outputNames[i]]);
-    if (build.status !== 0) {
-      console.error(`tailwindcss-build.js: Failed to build "${fileNames[i]}" into "${outputNames[i]}".`);
-      return false;
+  let failed = false;
+  for (const fileName of fileNames) {
+    if (indexPattern.test(fileName)) {
+      const buildResult = build(fileName, fileName.replace(indexPattern, 'index.css'));
+      failed = failed || !buildResult;
+    } else if (normalPattern.test(fileName)) {
+      const buildResult = build(fileName, fileName.replace(normalPattern, 'css'));
+      failed = failed || !buildResult;
+    } else {
+      printError(`Skipped "${fileName}".`);
     }
   }
 
-  return true;
+  return !failed;
 }
 
 function tailwindcssBuild() {
-  const buildIndexSuccess = buildIndex();
-  if (!buildIndexSuccess) {
-    return 1;
+  if (process.argv.length >= 3) {
+    const [ , , ...fileNames ] = process.argv;
+    const buildSuccess = tryBuild(fileNames);
+    return buildSuccess ? 0 : 1;
   }
 
-  const buildOthersSuccess = buildOthers();
-  if (!buildOthersSuccess) {
-    return 1;
+  const fileNames = [
+    ...glob.sync('src/**/tailwind.css'),
+    ...glob.sync('src/**/*.tailwind.css'),
+  ];
+  const buildResult = tryBuild(fileNames);
+  if (buildResult !== 0) {
+    printError('Build failed!');
   }
 
-  console.error('tailwindcss-build.js: Build complete.');
+  printError('Build complete successfully!');
   return 0;
 }
 
