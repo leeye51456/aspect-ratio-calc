00662b2970a639b4b88fe868e8521db33ae0227f
leeye51456 <leeye51456@gmail.com>
2020-10-07 20:59:00 +0900

    Add basic layout for App

--- src/components/layout/App.test.tsx
+++ src/components/layout/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import App from './App';
 
-test('renders empty App component', () => {
-  const { getByTestId } = render(<App />);
-  const renderedComponent = getByTestId('App');
-  expect(renderedComponent).toHaveClass('App');
+test('components/layout/App: Render App component', () => {
+  const renderedComponent: RenderResult = render(<App />);
+
+  expect(renderedComponent.getByTestId('App')).toHaveClass('App');
+
+  const headerTitle: HTMLElement = renderedComponent.getByText(/aspect ratio calculator/i);
+  expect(headerTitle).toBeInTheDocument();
+
+  const button: HTMLElement = renderedComponent.getByText(/^add$/i);
+  expect(button).toBeInTheDocument();
 });
--- src/components/layout/App.tsx
+++ src/components/layout/App.tsx
@@ -3,6 +3,25 @@ import React from 'react';
 function App() {
   return (
     <div className="App" data-testid="App">
+      <header className="App-header">
+        <h1 className="App-header-title">
+          Aspect Ratio Calculator
+        </h1>
+      </header>
+
+      <main className="App-main">
+        {/* TODO - Place screenForms: JSX.Element[] here. ScreenForm's below are for testing CSS. */}
+
+        <div className="App-main-add">
+          <div className="App-main-add-ratio" />
+          <button
+            className="App-main-add-button"
+            type="button"
+          >
+            Add
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
