99d918767455ffa9f5d3706e3a8166d937833345
leeye51456 <leeye51456@gmail.com>
2020-10-11 03:49:01 +0900

    Implement adding ScreenForm into App and its test

--- src/components/layout/App.test.tsx
+++ src/components/layout/App.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import 'react-dom/test-utils';
 import { render, RenderResult } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('components/layout/App: Render App component', () => {
@@ -12,4 +14,28 @@ test('components/layout/App: Render App component', () => {
 
   const button: HTMLElement = renderedComponent.getByText(/^add$/i);
   expect(button).toBeInTheDocument();
+});
+
+test('components/layout/App: Click add button', () => {
+  const renderedComponent: RenderResult = render(<App />);
+
+  const addButton: HTMLElement = renderedComponent.getByText(/^add$/i);
+  expect(addButton).toBeInTheDocument();
+
+  userEvent.click(addButton);
+
+  const widthInput: HTMLElement = renderedComponent.getByDisplayValue(/^1920$/);
+  const heightInput: HTMLElement = renderedComponent.getByDisplayValue(/^1080$/);
+  const diagonalInput: HTMLElement = renderedComponent.getByDisplayValue(/^24$/);
+  const aspectRatioText: HTMLElement = renderedComponent.getByText(/^aspect ratio$/i);
+  const dpiText: HTMLElement = renderedComponent.getByText(/^dpi$/i);
+  const dotPitchText: HTMLElement = renderedComponent.getByText(/^dot pitch$/i);
+  const pixelCountText: HTMLElement = renderedComponent.getByText(/^pixel count$/i);
+  expect(widthInput.title).toMatch(/^width$/i);
+  expect(heightInput.title).toMatch(/^height$/i);
+  expect(diagonalInput.title).toMatch(/^diagonal$/i);
+  expect(aspectRatioText).toBeInTheDocument();
+  expect(dpiText).toBeInTheDocument();
+  expect(dotPitchText).toBeInTheDocument();
+  expect(pixelCountText).toBeInTheDocument();
 });
--- src/components/layout/App.tsx
+++ src/components/layout/App.tsx
@@ -1,7 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactSetState from '../../utils/ReactSetState';
+import ScreenForm, { ScreenFormPropName } from '../forms/ScreenForm';
 import './App.css';
 
+interface ScreenFormProps {
+  id: number,
+  width: string,
+  height: string,
+  diagonal: string,
+}
+
+interface ScreenFormData {
+  [id: number]: ScreenFormProps,
+}
+
+interface IndexById {
+  [id: number]: number,
+}
+
+interface AddNewScreenFormParam {
+  screenData: ScreenFormData,
+  setScreenData: ReactSetState<ScreenFormData>,
+  screenIdOrder: number[],
+  setScreenIdOrder: ReactSetState<number[]>,
+  indexById: IndexById,
+  setIndexById: ReactSetState<IndexById>,
+  nextId: number,
+  setNextId: ReactSetState<number>,
+}
+
+const addNewScreenForm = function addNewScreenFormToApp(
+  {
+    screenData,
+    setScreenData,
+    screenIdOrder,
+    setScreenIdOrder,
+    indexById,
+    setIndexById,
+    nextId,
+    setNextId,
+  }: AddNewScreenFormParam
+): void {
+  const id = nextId;
+  setNextId(nextId + 1);
+
+  const newScreenFormProps: ScreenFormProps = {
+    id,
+    width: '1920',
+    height: '1080',
+    diagonal: '24',
+  };
+
+  const nextScreenData: ScreenFormData = { ...screenData, [id]: newScreenFormProps };
+  setScreenData(nextScreenData);
+
+  const nextIndexById: IndexById = { ...indexById, [id]: screenIdOrder.length };
+  setIndexById(nextIndexById);
+
+  const nextScreenIdOrder: number[] = [ ...screenIdOrder, id ];
+  setScreenIdOrder(nextScreenIdOrder);
+};
+
 function App() {
+  const [ screenData, setScreenData ] = useState<ScreenFormData>({});
+  const [ screenIdOrder, setScreenIdOrder ] = useState<number[]>([]);
+  const [ indexById, setIndexById ] = useState<IndexById>({});
+  const [ nextId, setNextId ] = useState(0);
+
+  const handleAddClick = function handleAddNewScreenFormClick(): void {
+    addNewScreenForm(
+      { screenData, setScreenData, screenIdOrder, setScreenIdOrder, indexById, setIndexById, nextId, setNextId }
+    );
+  };
+
+  const screenForms = screenIdOrder.map((id) => {
+    const onChange = function handleScreenFormChange(
+      id: number,
+      propName: ScreenFormPropName,
+      propValue: string,
+    ): void {
+      const nextScreenFormProps: ScreenFormProps = { ...screenData[id], [propName]: propValue };
+      const nextScreenData: ScreenFormData = { ...screenData, [id]: nextScreenFormProps };
+      setScreenData(nextScreenData);
+    };
+    return <ScreenForm { ...screenData[id] } onChange={onChange} key={id} />;
+  });
+
   return (
     <div className="App" data-testid="App">
       <header className="App-header">
@@ -11,13 +95,14 @@ function App() {
       </header>
 
       <main className="App-main">
-        {/* TODO - Place screenForms: JSX.Element[] here. ScreenForm's below are for testing CSS. */}
+        {screenForms}
 
         <div className="App-main-add">
           <div className="App-main-add-ratio" />
           <button
             className="App-main-add-button"
             type="button"
+            onClick={handleAddClick}
           >
             Add
           </button>
