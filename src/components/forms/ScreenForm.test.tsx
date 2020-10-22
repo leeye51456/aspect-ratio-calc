c37724b07a14a9c9f08c99e569ba54b864c8f0bf
leeye51456 <leeye51456@gmail.com>
2020-10-22 21:20:44 +0900

    Update test code for ScreenForm

--- src/components/forms/ScreenForm.test.tsx
+++ src/components/forms/ScreenForm.test.tsx
@@ -8,14 +8,10 @@ test('components/forms/ScreenForm: Render ScreenForm component', () => {
     width: '1920',
     height: '1080',
     diagonal: '24',
-    onChange: (id: number, propName: ScreenFormPropName, propValue: string): void => {},
-    onRemove: (id: number): void => {},
+    onChange: (): void => {},
+    onRemove: (): void => {},
   };
   const renderResult = render(<ScreenForm { ...props } />);
-
-  expect(renderResult.container.firstChild).toHaveClass('ScreenForm');
-  expect(renderResult.container.firstChild).toHaveStyle('width: 100%');
-  expect(renderResult.container.firstChild.firstChild).toHaveStyle('padding-bottom: 56.25%');
 
   expect(renderResult.getByDisplayValue(props.width)).toHaveAttribute('title', 'Width');
   expect(renderResult.getByDisplayValue(props.height)).toHaveAttribute('title', 'Height');
