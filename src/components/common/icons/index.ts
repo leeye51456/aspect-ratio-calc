a2b445c550064a4259d0b33338714befbefdbfbd
leeye51456 <leeye51456@gmail.com>
2020-10-28 22:33:24 +0900

    Add icon collection

---
+++ src/components/common/icons/index.ts
@@ -0,0 +1,21 @@
+import add from './add.svg';
+import copy from './copy.svg';
+import remove from './remove.svg';
+import rotate from './rotate.svg';
+
+// Hardcoded for auto completion
+interface IconCollection {
+  add: string,
+  copy: string,
+  remove: string,
+  rotate: string,
+}
+
+const icons: IconCollection = {
+  add,
+  copy,
+  remove,
+  rotate,
+};
+
+export default icons;
