b263184b7d1cbd4da6b38a754cec377619667fb5
leeye51456 <leeye51456@gmail.com>
2020-10-29 22:08:38 +0900

    Deploy 1.0.0

---
+++ precache-manifest.c67824480dcccc303f2d04480ebfc8f9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2671d107a1397482e14108dc7a4b9db1",
+    "url": "/aspect-ratio-calc/index.html"
+  },
+  {
+    "revision": "f00b49e66ff3de238e0f",
+    "url": "/aspect-ratio-calc/static/css/main.16f380f1.chunk.css"
+  },
+  {
+    "revision": "e4505e663e65aca8dba1",
+    "url": "/aspect-ratio-calc/static/js/2.f601bb71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aspect-ratio-calc/static/js/2.f601bb71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00b49e66ff3de238e0f",
+    "url": "/aspect-ratio-calc/static/js/main.d72fb035.chunk.js"
+  },
+  {
+    "revision": "2b397558e14061fdde47",
+    "url": "/aspect-ratio-calc/static/js/runtime-main.be6fada4.js"
+  },
+  {
+    "revision": "0e3e08bcb441c2cb39bdfff7f56f06a2",
+    "url": "/aspect-ratio-calc/static/media/add.0e3e08bc.svg"
+  },
+  {
+    "revision": "72dd793a97203adfad3c50ebad310ee4",
+    "url": "/aspect-ratio-calc/static/media/copy.72dd793a.svg"
+  },
+  {
+    "revision": "a24d83e3452a8bfeefadc83930409188",
+    "url": "/aspect-ratio-calc/static/media/remove.a24d83e3.svg"
+  },
+  {
+    "revision": "c91fa0034454b1924164962ab36bb5e5",
+    "url": "/aspect-ratio-calc/static/media/rotate.c91fa003.svg"
+  }
+]);
