7c4f67092f77a4874c9a16055ec2899fdd501c51
leeye51456 <leeye51456@gmail.com>
2020-11-06 22:41:05 +0900

    Add unit conversion function to ScreenInfo

--- src/utils/ScreenInfo.ts
+++ src/utils/ScreenInfo.ts
@@ -13,6 +13,14 @@ export interface RectSize {
 
 const INCH_TO_CENTIMETER_FACTOR: number = 2.54;
 
+export const toInches = function convertCentimetersToInches(centimeters: number): number {
+  return centimeters / INCH_TO_CENTIMETER_FACTOR;
+};
+
+export const toCentimeters = function convertInchesToCentimeters(inches: number): number {
+  return inches * INCH_TO_CENTIMETER_FACTOR;
+};
+
 export class ScreenInfoBase {
   protected map: Map<string, string> | null;
 
@@ -66,7 +74,7 @@ export class ScreenInfoWithDiagonal extends ScreenInfoBase {
 
     this.diagonal = diagonal;
     this.dpi = Math.sqrt(this.pixelCount.width ** 2 + this.pixelCount.height ** 2) / diagonal;
-    this.dotPitch = 10 * INCH_TO_CENTIMETER_FACTOR / this.dpi;
+    this.dotPitch = 10 * toCentimeters(1 / this.dpi);
     this.size = {
       width: this.pixelCount.width * this.dotPitch / 10,
       height: this.pixelCount.height * this.dotPitch / 10,
