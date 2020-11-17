10b4692faf9e5edebe01b9f3e6befcef4b4e1aef
leeye51456 <leeye51456@gmail.com>
2020-11-18 00:28:51 +0900

    Remove line breaks from import

--- src/components/layout/App.tsx
+++ src/components/layout/App.tsx
@@ -2,11 +2,7 @@ import React, { useState } from 'react';
 import useScreenData, { ScreenFormData } from '../../hooks/useScreenData';
 import copyToClipboard from '../../utils/copyToClipboard';
 import { toFixedWithoutTrailingZero, tryParsePositiveFloat } from '../../utils/number';
-import {
-  AvailableUnit,
-  toCentimeters,
-  toInches,
-} from '../../utils/ScreenInfo';
+import { AvailableUnit, toCentimeters, toInches } from '../../utils/ScreenInfo';
 import { getWholeYaml } from '../../utils/yaml';
 import ScreenForm, { ScreenFormChangedProps } from '../forms/ScreenForm';
 import ToggleSwitch from '../forms/ToggleSwitch';
